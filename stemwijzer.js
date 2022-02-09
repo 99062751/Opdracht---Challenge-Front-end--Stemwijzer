6fdf1d6ef0be19e01f8b4e77b97c024c9cf11589
99062751 <70890704+99062751@users.noreply.github.com>
2022-02-09 08:59:13 +0100

    werk van dins 8 februari
    bezig geweest ontwerp na te maken, met de blokken en ben bezig met grid. tekst staat er een beetje bij en heb een paar dingen toegevoegd zodat ik er al een styling voor kan maken

---
+++ stemwijzer.js
@@ -0,0 +1,5 @@
+var vote_container= document.getElementById("vote_container");
+var desc_container= document.getElementById("desc_container");
+var party_container= document.getElementById("party_container");
+
+
